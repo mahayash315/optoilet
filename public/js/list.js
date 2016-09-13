a558c5490ba2c16c5e8c46f197645ab42b03ceb2
recruithd-103 <recruithd_103@r.recruit.co.jp>
2016-09-13 17:37:48 +0900

    仮commit

--- public/js/list.js
+++ public/js/list.js
@@ -1,6 +1,7 @@
 var vm = new Vue({
     el: '#list',
     data: {
+        inSearch: false,
         toilets: [
             {
                 "id": 1,
@@ -40,7 +41,11 @@ var vm = new Vue({
                     }
                 ]
             }
-        ]
+        ],
+        costs: {
+            1: 0,
+            2: 3
+        }
     },
     created: function() {
         var getUrlVars = function() {
@@ -59,34 +64,71 @@ var vm = new Vue({
         var cf = args.currentFloor;
         var dir = args.direction;
         // API call
-        $.ajax({
-            method: 'GET',
-            url: '//localhost:3000/api/toilets'
-        })
-        .done(function(data) {
-            vm.toilets = data
-                .filter(function (toilet) { return (toilet.gender === 'male'); })
-                .reverse();
-            if (cf) {
-                var idx = -1;
-                $.each(vm.toilets, function(i, toilet) {
-                    if (toilet.floor == cf) {
-                        idx = toilet.id;
-                    }
-                });
-                if (idx !== -1) {
-                    setTimeout(function() {
-                        $("html,body").animate({
-                            scrollTop: $('#heading-'+idx).offset().top - 240
-                        });
-                        $('#heading-'+idx).parent().addClass('panel-info');
-                    }, 100);
+        if (cf) {
+            this.inSearch = true;
+            $.ajax({
+                method: 'GET',
+                url: '//localhost:3000/api/toilets/search',
+                data: {
+                    current_floor: cf,
+                    direction: dir
                 }
-            }
-        })
-        .fail(function() {
-            alert('load failed');
-        });
+            })
+            .done(function(data) {
+                vm.toilets = data.toilets
+                    .filter(function (toilet) { return (toilet.gender === 'male'); })
+                    .reverse();
+                vm.costs = data.costs;
+                if (cf) {
+                    var idx = -1;
+                    $.each(vm.toilets, function(i, toilet) {
+                        if (toilet.floor == cf) {
+                            idx = toilet.id;
+                        }
+                    });
+                    if (idx !== -1) {
+                        setTimeout(function() {
+                            $("html,body").animate({
+                                scrollTop: $('#heading-'+idx).offset().top - 240
+                            });
+                            $('#heading-'+idx).parent().addClass('panel-info');
+                        }, 100);
+                    }
+                }
+            })
+            .fail(function() {
+                alert('load failed');
+            });
+        } else {
+            $.ajax({
+                method: 'GET',
+                url: '//localhost:3000/api/toilets'
+            })
+            .done(function(data) {
+                vm.toilets = data
+                    .filter(function (toilet) { return (toilet.gender === 'male'); })
+                    .reverse();
+                if (cf) {
+                    var idx = -1;
+                    $.each(vm.toilets, function(i, toilet) {
+                        if (toilet.floor == cf) {
+                            idx = toilet.id;
+                        }
+                    });
+                    if (idx !== -1) {
+                        setTimeout(function() {
+                            $("html,body").animate({
+                                scrollTop: $('#heading-'+idx).offset().top - 240
+                            });
+                            $('#heading-'+idx).parent().addClass('panel-info');
+                        }, 100);
+                    }
+                }
+            })
+            .fail(function() {
+                alert('load failed');
+            });
+        }
     },
     ready: function() {
     },
