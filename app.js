0c7163eb44a94409a59121c8a4934b2205d57351
Masayuki <hayashi.masayuki@itolab.nitech.ac.jp>
2016-09-13 12:50:12 +0900

    [update] updated app.js

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mysql = require('mysql');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var api_toilets = require('./routes/api/toilets');
 
 var app = express();
 
@@ -22,9 +23,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+// ======================= APP =========================
 
+app.use('/', routes);
+app.use('/api/toilets', api_toilets);
+
+
+// ================== ERROR HANDLER ====================
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
